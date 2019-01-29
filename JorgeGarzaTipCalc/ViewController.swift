5ae46b539c0737a45d14fc6460c0366b813ea774
Garza Garcia, Jorge M <jmg7276@lib-pclem044.local>
2019-01-28 20:10:26 -0600

    Initial Commit

---
+++ JorgeGarzaTipCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JorgeGarzaTipCalc
+//
+//  Created by Garza Garcia, Jorge M on 1/28/19.
+//  Copyright © 2019 Garza Garcia, Jorge M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
